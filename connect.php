63cf0b6730bc6a56d8715f0362f4f21dbbcd976b
hu0599 <7ussein.5alil@gmail.com>
2020-04-02 16:52:02 -0400

    class Database und function connect to DB added

--- connect.php
+++ connect.php
@@ -1,2 +1,19 @@
 <?php
 
+class Database
+{
+    private function connect()
+    {
+        $dsn = "mysql:host=localhost;dbname=db;charset=utf8mb4";
+        $username = 'admin';
+        $password = 'pass123';
+
+        $options = [
+            PDO::ATTR_EMULATE_PREPARES   => false,                  // turn off emulation mode
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // turn on errors
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // make the default fetch an associative array
+        ];
+
+        return new PDO($dsn, "$username", "$password", $options);
+    }
+}
